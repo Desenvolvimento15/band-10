66c4e60eaf68dfe5dbb928fe18070ce7c72fbe14
Desenvolvimento15 <desenvolvimento15@dl.com.br>
2025-07-25 16:20:17 -0300

    vs01

---
+++ main.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function(){
+    $('.slick-track').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      adaptiveHeight: true
+    });
+  });
+
