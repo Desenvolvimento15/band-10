f9047847aa942477148af42e7aac6702e9303010
Desenvolvimento15 <desenvolvimento15@dl.com.br>
2025-08-06 10:57:21 -0300

    vs01

--- main.js
+++ main.js
@@ -12,3 +12,23 @@
     });
   });
 
+ gsap.registerPlugin(ScrollTrigger);
+
+ const container = document.querySelector(".scroll-horizontal-content");
+ const sections = gsap.utils.toArray(".pad15");
+ const totalSections = sections.length;
+ const containerWidth = 200 * totalSections;
+
+ gsap.set(container, { width: `${containerWidth}vw` });
+
+ gsap.to(container, {
+   xPercent: -200 * (totalSections - 1),
+   ease: "none",
+   scrollTrigger: {
+     trigger: ".horizontal-scroll-wrapper",
+     pin: true,
+     scrub: 1,
+     snap: 1 / (totalSections - 1),
+     end: () => "+=" + window.innerWidth * (totalSections - 1),
+   },
+ });
